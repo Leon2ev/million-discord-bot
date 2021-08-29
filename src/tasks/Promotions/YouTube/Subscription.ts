5c47523ef5f3b887cb5040ae6783ffc8e80d4a73
Leon2ev <jevgeni.glazunov@gmail.com>
2021-08-29 23:53:40 +0200

    Added youtube notification webhook. Add/delete/find youtube subscriptions features. Airtable connected.

---
+++ src/tasks/Promotions/YouTube/Subscription.ts
@@ -0,0 +1,35 @@
+import nodeFetch, { Response } from 'node-fetch';
+import * as FormData from 'form-data';
+
+export class YouTubeSubscription {
+  static pshbUrl = 'https://pubsubhubbub.appspot.com';
+  static topicUrl = 'https://www.youtube.com/xml/feeds/videos.xml?channel_id=';
+  static hostname = process.env.HOSTNAME;
+
+  static async sendPshbRequest(channelId: string, mode: string): Promise<Response | void>{
+    try {
+      if (!this.hostname) {
+        throw {
+          title: 'PubSubHubBub Error',
+          message: 'Subscription will work only on production environment'
+        }
+      }
+      const callbackUrl = `https://${this.hostname}/youtube/notification`;
+    
+      const form = new FormData();
+    
+      form.append('hub.callback', callbackUrl);
+      form.append('hub.topic', this.topicUrl + channelId);
+      form.append('hub.verify', 'async');
+      form.append('hub.mode', mode);
+    
+      return await nodeFetch(this.pshbUrl, {
+        method: 'POST',
+        body: form,
+      });
+
+    } catch(e) {
+      console.error(e);
+    }
+  }
+}
