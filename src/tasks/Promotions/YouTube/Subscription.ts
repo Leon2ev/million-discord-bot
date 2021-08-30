fb337ad35c35847c5c1b98b8637c7a2d1ca144d3
Leon2ev <jevgeni.glazunov@gmail.com>
2021-08-30 23:04:02 +0200

    fix

--- src/tasks/Promotions/YouTube/Subscription.ts
+++ src/tasks/Promotions/YouTube/Subscription.ts
@@ -2,26 +2,27 @@ import nodeFetch, { Response } from 'node-fetch';
 import * as FormData from 'form-data';
 
 export class YouTubeSubscription {
-  static pshbUrl = 'https://pubsubhubbub.appspot.com';
-  static topicUrl = 'https://www.youtube.com/xml/feeds/videos.xml?channel_id=';
-
   static async sendPshbRequest(channelId: string, mode: string): Promise<Response | void>{
     try {
+
       if (process.env.NODE_ENV !== 'production') {
         throw {
           title: 'PubSubHubBub Error',
           message: 'Subscription will work only on production environment'
         }
       }
+      
+      const pshbUrl = 'https://pubsubhubbub.appspot.com';
+      const topicUrl = 'https://www.youtube.com/xml/feeds/videos.xml?channel_id=';
       const callbackUrl = `https://youtube-notification-test-app.herokuapp.com/youtube/notification`;
     
       const form = new FormData();
     
       form.append('hub.callback', callbackUrl);
-      form.append('hub.topic', this.topicUrl + channelId);
+      form.append('hub.topic', topicUrl + channelId);
       form.append('hub.mode', mode);
     
-      return await nodeFetch(this.pshbUrl, {
+      return await nodeFetch(pshbUrl, {
         method: 'POST',
         body: form,
       });
